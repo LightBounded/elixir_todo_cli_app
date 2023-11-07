3583ebf55fead03fc8fb62e7cc56b8c4540f09c0
Caleb Rivera <calebrossr@gmail.com>
2023-11-07 05:57:54 -0500

    format

--- main.exs
+++ main.exs
@@ -1,4 +1,3 @@
-
 defmodule Todo do
   def start do
     loop([])
@@ -6,6 +5,7 @@ defmodule Todo do
 
   defp loop(list) do
     display_menu()
+
     case read_input() do
       "1" -> loop(add(list))
       "2" -> loop(list(list))
@@ -17,7 +17,7 @@ defmodule Todo do
   end
 
   defp display_menu do
-    IO.puts """
+    IO.puts("""
     Todo List App
 
     1. Add item
@@ -25,40 +25,45 @@ defmodule Todo do
     3. Complete item
     4. Delete item
     5. Quit
-    """
+    """)
   end
 
   defp read_input do
-    IO.puts "Enter your choice: "
+    IO.puts("Enter your choice: ")
     IO.gets("") |> String.trim()
   end
 
   defp add(list) do
-    IO.puts "Enter new item: "
+    IO.puts("Enter new item: ")
     item = IO.gets("") |> String.trim()
     [item | list]
   end
 
   defp list(list) do
-    IO.puts "Todo List:"
-    Enum.each(list, fn item -> IO.puts "- #{item}" end)
+    IO.puts("Todo List:")
+    Enum.each(list, fn item -> IO.puts("- #{item}") end)
     list
   end
 
   defp complete(list) do
-    IO.puts "Enter item number to complete: "
+    IO.puts("Enter item number to complete: ")
     index = IO.gets("") |> String.trim() |> String.to_integer()
-    Enum.with_index(list) |> Enum.map(fn {item, i} -> if i == index - 1, do: "[x] #{item}", else: item end)
+
+    Enum.with_index(list)
+    |> Enum.map(fn {item, i} -> if i == index - 1, do: "[x] #{item}", else: item end)
   end
 
   defp delete(list) do
-    IO.puts "Enter item number to delete: "
+    IO.puts("Enter item number to delete: ")
     index = IO.gets("") |> String.trim() |> String.to_integer()
-    Enum.with_index(list) |> Enum.reject(fn {_, i} -> i == index - 1 end) |> Enum.map(fn {item, _} -> item end)
+
+    Enum.with_index(list)
+    |> Enum.reject(fn {_, i} -> i == index - 1 end)
+    |> Enum.map(fn {item, _} -> item end)
   end
 
   defp quit do
-    IO.puts "Goodbye!"
+    IO.puts("Goodbye!")
     :ok
   end
 end
